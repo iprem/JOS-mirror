d035ef70e6733d9fbc0d81832a356f57c6204cd0
Leixiang Wu <leixiang.wu@stonybrook.edu>
2016-02-12 00:14:18 -0500

    Finish rest of exercises

--- kern/monitor.c
+++ kern/monitor.c
@@ -26,6 +26,7 @@ struct Command {
 static struct Command commands[] = {
 	{ "help", "Display this list of commands", mon_help },
 	{ "kerninfo", "Display information about the kernel", mon_kerninfo },
+	{ "backtrace", "Display a listing of function call frames", mon_backtrace }
 };
 #define NCOMMANDS (sizeof(commands)/sizeof(commands[0]))
 
@@ -60,7 +61,37 @@ mon_kerninfo(int argc, char **argv, struct Trapframe *tf)
 int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
+	int x = 100;
 	// Your code here.
+	cprintf("Stack backtrace:\n");
+	uint64_t stack_base_p = read_rbp();
+	uint64_t ret_inst_p;
+	struct Ripdebuginfo rip_debug_info;
+	read_rip(ret_inst_p);
+	while(stack_base_p != 0) {
+		cprintf("%5s %016x%5s %016x\n", "rbp", stack_base_p, 
+			"rip", ret_inst_p);
+		if (debuginfo_rip(ret_inst_p, &rip_debug_info) == 0) {
+			// Print th file name and line within that file of the stack frame's rip
+			cprintf("%7s%s:%d: ", " ", rip_debug_info.rip_file, rip_debug_info.rip_line);
+			// Print the name of the function
+			cprintf("%.*s+", rip_debug_info.rip_fn_namelen, rip_debug_info.rip_fn_name);
+			// Print the offset of the rip from the first instruction of the function
+			cprintf("%016x", ret_inst_p - rip_debug_info.rip_fn_addr);
+			// Print the number of function arguments
+			cprintf("  args:%d ", rip_debug_info.rip_fn_narg);
+			// Print the actual arguments themselves
+			int arg_num;
+			uint64_t arg_addr = stack_base_p;
+			for (arg_num = 0; arg_num < rip_debug_info.rip_fn_narg; arg_num++) {
+				arg_addr -= rip_debug_info.size_fn_arg[arg_num];
+				cprintf(" %016x", *(uint32_t *)arg_addr);
+			}
+			cprintf("\n");
+		}
+		ret_inst_p = *((uint64_t *)(stack_base_p + 1 * sizeof(uint64_t)));
+		stack_base_p = *(uint64_t *)stack_base_p;
+	}
 	return 0;
 }
 
